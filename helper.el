91bfe6e047b0abb86ad98b1adfe65be647cfbe00
Drew <adluther@mail.roanoke.edu>
2018-02-01 18:39:43 -0500

    length, calcNetProfit, sumStock

--- helper.el
+++ helper.el
@@ -1,3 +1,11 @@
+;;PRE:  L is a list
+;;POST: RV = N where N is the number of elements in L
+(defun length (L)
+  (cond ((equal L nil) 0)
+	(t (+ (length (cdr L)) 1))
+  )
+)
+
 ;;PRE:  inventory type
 ;;POST: RV = inventory type sorted in descending order
 (defun inventorySort (inventory)
@@ -5,13 +13,20 @@
 )
 
 ;;PRE:  warehouse which equals (IID WID CS MS CPI PPI)
-;;POST: RV = PPI iff CS < MS, otherwise PPI-CPI
+;;POST: RV = PPI-CPI iff CS < MS, otherwise PPI
 (defun calcNetProfit (warehouse)
-  ;; pass
+  (cond ((< (nth 2 warehouse) (nth 3 warehouse))
+	 ;;position 2 is the CS and position 3 is the MS
+	 (- (nth 5 warehouse) (nth 4 warehouse)))
+	;;position 5 is PPI and position 4 is CPI
+	;;Since CS is below MS, the net profit is PPI - CPI
+	(t (nth 5 warehouse))
+	)
 )
 
 ;;PRE:  inventory type
 ;;POST: RV = sum of stock in inventory type
 (defun sumStock (inventory)
-  ;; pass
+  (+ (sumStock (cdr inventory)) (nth 2 (car inventory))
+     ;;Element of a warehouse at position 2 is CS
 )
