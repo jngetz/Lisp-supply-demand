4b01c839ea48ed7ff97c53d20fca6823f1ca5308
jngetz <jngetz@mail.roanoke.edu>
2018-02-04 22:33:39 -0500

    some minor progress
    might be reverted

--- fillItemOrder.el
+++ fillItemOrder.el
@@ -45,11 +45,30 @@
   (shipStockHelper inventory request '(() ())) ; empty f.o.t. passed in
 )
 
-;;PRE:  intentory type, number of items requested, basis is final output type list to build into
+;;PRE:  inventory type, number of items requested, basis is final output type list to build into
 ;;POST: RV = final output type
 (defun shipStockHelper (inventory request basis)
   (cond
     ((equal request 0) basis) ; when the request is filled return the built basis list
-    (t (shipStockHelper (addWarehouse (cdr inventory) (updateWarehouse (car inventory) N)) (- N (supplyFrom (car inventory))) ())) ;TODO: finish the recursive call
+    (t (shipStockHelper (addWarehouse (cdr inventory) (updateWarehouse (car inventory) N)) (- N (supplyFrom (car inventory) N)) (list (append ## (car basis)) (append ## (last basis))))) ;TODO: finish the recursive call
+  )
+)
+
+;;PRE:  the warehouse needing to be updated and the current N being worked with
+;;POST: a 6-tuple (IID WID CS MS CPI PPI) where CS is updated to CS - min(CS, N)
+;;      iff CS <= MS, otherwise CS updated to CS - min(CS-MS, N)
+(defun updateWarehouse (warehouse N)
+  (cond
+    ((> (nth 2 warehouse) (nth 3 warehouse)) (append (list (nth 0 warehouse) (nth 1 warehouse) (- (nth 2 warehouse) (min (- (nth 2 warehouse) (nth 3 warehouse)) N))) (nthcdr 3 warehouse)))
+    (t (append (list (nth 0 warehouse) (nth 1 warehouse) (- (nth 2 warehouse) (min (nth 2 warehouse) N))) (nthcdr 3 warehouse)))
+  )
+)
+
+;;PRE:  warehouse we are extracting from and the current N
+;;POST: the number of items taken from the warehouse
+(defun supplyFrom (warehouse N)
+  (cond
+    ((> (nth 2 warehouse) (nth 3 warehouse)) (min (- (nth 2 warehouse) (nth 3 warehouse)) N))
+    (t (min (nth 2 warehouse) N))
   )
 )
