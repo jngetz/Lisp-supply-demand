a944c59409dd01a248b739f1ba265efb9ea0d050
jngetz <jngetz@mail.roanoke.edu>
2018-02-04 20:07:03 -0500

    fillItemOrder near completion
    getWarehouses working
    addWarehouse working
    shipStock and fillItemOrder handtraced
    shipStockHelper needs recursive case finished

--- fillItemOrder.el
+++ fillItemOrder.el
@@ -4,14 +4,27 @@
 ;;      item_demand is a pair (IID R) where R is the number of items requested
 ;;POST: RV = final output type
 (defun fillItemOrder (inventory item_demand)
-  ;; pass
+  (cond
+    ((< (sumStock (getWarehouses (car item_demand))) (car (last item_demand))) '((((car item_demand) () 0 nil)) ()))
+    (t (shipStock (getWarehouses (car item_demand)) (car (last item_demand))))
+  )
 )
 
-;;PRE:  iid exists in the inventory type and sortedInventory is inventory type
-;;      sorted in descending order
+;;PRE:  iid exists in the inventory type and inventory is inventory type
 ;;POST: RV = inventory type where all IID = iid
-(defun getWarehouses (iid sortedInventory)
-  ;; pass
+(defun getWarehouses (iid inventory)
+  (getWarehousesHelper iid inventory nil)
+)
+
+;;PRE:  the iid to include, inventory type to read from, basis for the building
+;;      of the RV
+;;POST: RV = inventory type where all IID = iid
+(defun getWarehousesHelper (iid inventory basis)
+  (cond
+    ((equal inventory nil) basis)
+    ((equal iid (car (car inventory))) (getWarehousesHelper iid (cdr inventory) (addWarehouse basis (car inventory))))
+    (t (getWarehousesHelper iid (cdr inventory) basis))
+  )
 )
 
 ;;PRE:  inventory type and a warehouse which equals (IID WID CS MS CPI PPI)
@@ -19,7 +32,6 @@
 ;;POST: RV = inventory type where warehouse is in inventory and inventory is
 ;;      sorted in descending order of net profit on next sale
 (defun addWarehouse (inventory warehouse)
-  ;; pass
   (cond
     ((equal inventory nil) (append inventory (list warehouse)))
     ((> (calcNetProfit warehouse) (calcNetProfit (car inventory))) (append (list warehouse) inventory))
@@ -27,8 +39,17 @@
   )
 )
 
-;;PRE:  inventory type and R, the number of items requested
+;;PRE:  inventory type and request, the number of items requested
 ;;POST: RV = final output type
 (defun shipStock (inventory request)
-  ;; pass
+  (shipStockHelper inventory request '(() ())) ; empty f.o.t. passed in
+)
+
+;;PRE:  intentory type, number of items requested, basis is final output type list to build into
+;;POST: RV = final output type
+(defun shipStockHelper (inventory request basis)
+  (cond
+    ((equal request 0) basis) ; when the request is filled return the built basis list
+    (t (shipStockHelper (addWarehouse (cdr inventory) (updateWarehouse (car inventory) N)) (- N (supplyFrom (car inventory))) ())) ;TODO: finish the recursive call
+  )
 )
